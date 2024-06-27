083dc888b31b6480dea0ff1b01f8f4d96160e057
mufaddal Hatim <mufaddalhatim53@gmail.com>
2024-06-27 12:51:04 +0500

    fix: login attempt retries

--- app/user/user.py
+++ app/user/user.py
@@ -15,8 +15,11 @@ router = APIRouter()
 
 logger = logging.getLogger("uvicorn.error")
 logger.setLevel(logging.DEBUG)
-# logger.addHandler(logging.StreamHandler())
 
+login_attempts: Dict[str, int] = {}
+lockout_expiry: Dict[str, datetime.datetime] = {}
+MAX_ATTEMPTS = 3
+LOCKOUT_TIME = datetime.timedelta(minutes=30)
 
 def get_db():
     db = _database.SessionLocal()
@@ -48,12 +51,23 @@ async def register_user(user: _schemas.UserCreate, db: _orm.Session = Depends(ge
 
 @router.post("/login")
 async def login(user: _schemas.GenerateUserToken,db: _orm.Session = Depends(get_db)):
-    logger.debug("Here 1", user.email, user.password)
-    authenticated_user = await _services.authenticate_user(user.email, user.password, db)
     
+    if user.email in lockout_expiry and datetime.datetime.now() < lockout_expiry[user.email]:
+        raise HTTPException(status_code=403, detail="Account locked. Try again later.")
+
+    authenticated_user = await _services.authenticate_user(user.email, user.password, db)
     if not authenticated_user:
-        raise HTTPException(status_code=400, detail="Invalid email or password")
-    token= await _services.create_token(authenticated_user)
+        login_attempts[user.email] = login_attempts.get(user.email, 0) + 1
+
+        if login_attempts[user.email] >= MAX_ATTEMPTS:
+            # Lock the account if maximum attempts are reached
+            lockout_expiry[user.email] = datetime.now() + LOCKOUT_TIME
+            raise HTTPException(status_code=403, detail="Account locked. Try again later.")
+
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    login_attempts[user.email] = 0
+    token = await _services.create_token(authenticated_user)
     user_data = await _services.get_alluser_data(email=user.email, db=db)
     return {
         "user": user_data,
