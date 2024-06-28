3a2df873cc5e355329b1d590ef75e7af546ca87d
hammadpasha12 <hammadqsol@gmail.com>
2024-06-28 12:16:42 +0500

    minor conflits

--- app/user/user.py
+++ app/user/user.py
@@ -7,15 +7,11 @@ import app.user.models as _models
 import app.user.service as _services
 # from main import logger
 import app.core.db.session as _database
-import logging, os
+import pika
+import logging
 import datetime
-from datetime import datetime as _dt
-import fastapi.security as _security
 
 router = APIRouter()
-
-JWT_SECRET = os.getenv("JWT_SECRET")
-oauth2schema = _security.OAuth2PasswordBearer(tokenUrl="/api/login")
 
 logger = logging.getLogger("uvicorn.error")
 logger.setLevel(logging.DEBUG)
@@ -32,12 +28,29 @@ def get_db():
     finally:
         db.close()
 
+@router.post("/register/admin")
+async def register_user(user: _schemas.UserCreate, db: _orm.Session = Depends(get_db)):
+    print("Here 1", user.email, user.password, user.username)
+    db_user = await _services.get_user_by_email(user.email, db)
+    print(f"User: {db_user}")
+    print("Here 2")
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    organization_details = _schemas.OrganizationCreate(org_name=user.org_name)
+    organization = await _services.create_organization(organization_details, db)
+
+    user_data = user.dict()
+    user_data['org_id'] = organization.id
+    user_data.pop('org_name')
+
+    user_register = _schemas.UserRegister(**user_data, date_created=datetime.datetime.utcnow())
+    new_user = await _services.create_user(user_register, db)
+    
+    return new_user
 
 @router.post("/login")
-async def login(
-        user: _security.OAuth2PasswordRequestForm = Depends(_schemas.GenerateUserToken),
-        db: _orm.Session = Depends(get_db)
-    ):
+async def login(user: _schemas.GenerateUserToken,db: _orm.Session = Depends(get_db)):
     
     if user.email in lockout_expiry and datetime.datetime.now() < lockout_expiry[user.email]:
         raise HTTPException(status_code=403, detail="Account locked. Try again later.")
@@ -48,7 +61,7 @@ async def login(
 
         if login_attempts[user.email] >= MAX_ATTEMPTS:
             # Lock the account if maximum attempts are reached
-            lockout_expiry[user.email] = datetime.datetime.now() + LOCKOUT_TIME
+            lockout_expiry[user.email] = datetime.now() + LOCKOUT_TIME
             raise HTTPException(status_code=403, detail="Account locked. Try again later.")
 
         raise HTTPException(status_code=401, detail="Invalid email or password")
@@ -60,17 +73,7 @@ async def login(
         "user": user_data,
         "token": token
     }
-
-
-@router.post("/test_token")
-async def test_token(
-        token: str,
-        db: _orm.Session = Depends(get_db)
-    ):
-    print("Token 1: ", token)
-    payload = _services.verify_jwt(token)
-    return payload
-
+    
 @router.get("/get_all_countries/", response_model=List[_schemas.CountryRead])
 async def read_countries(db: _orm.Session = Depends(get_db)):
     countries = _services.get_all_countries(db=db)
