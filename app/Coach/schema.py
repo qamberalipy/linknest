bd59fa643ac8e079d7bc210ba52a303053684c47
qamber-qsol <qamber.qsol@gmail.com>
2024-08-08 12:06:33 +0500

    Organization apis

--- app/Coach/schema.py
+++ app/Coach/schema.py
@@ -142,5 +142,13 @@ class CoachReadSchema(pydantic.BaseModel):
     class Config:
         from_attributes = True
         arbitrary_types_allowed=True
+        
+class CoachOrganizationResponse(pydantic.BaseModel):
+    id: int
+    name: Optional[str] = None
+    profile_img: Optional[str] = None
+
+    class Config:
+        from_attributes = True
 
    
