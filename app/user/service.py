c4f0940268829a5fa31a7cccac7f684f0908734f
MuhammadMillwala53 <muhammadmillwala.qsols@gmail.com>
2024-08-20 14:11:24 +0500

    Reset URL Changed for Live Env

--- app/user/service.py
+++ app/user/service.py
@@ -395,7 +395,7 @@ def generate_password_reset_html(name: str ,email :str ,gym_name: str, token: st
                                                                             <tbody>
                                                                                 <tr>
                                                                                     <td align="center" style="border-radius: 3px;" bgcolor="#77DD77">
-                                                                                        <a href="{LOCAL_BASE_URL}/reset_password/{token}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #77DD77; display: inline-block;">Reset Password</a>
+                                                                                        <a href="{BASE_URL}/reset_password/{token}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #77DD77; display: inline-block;">Reset Password</a>
                                                                                     </td>
                                                                                 </tr>
                                                                             </tbody>
